91c16088a3ddabe386c8db804b23c4ab033c8018
Scott Bowler <scott.bowler@learningfuze.com>
2019-08-31 20:00:00 +0000

    Completed initial setup with client, server, and DB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db');
 const ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3001;
 
@@ -10,11 +11,31 @@ if(ENV !== 'production') {
     app.use(cors());
 }
 
-app.get('/api/test', (req, res) => {
+app.use(express.json());
+
+app.post('/api/test', async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    const [result] = await db.execute('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)', [name, email, phone]);
+
+    res.send({
+        message: 'POST API test successful',
+        method: req.method,
+        url: req.url,
+        userId: result.insertId
+    });
+});
+
+app.get('/api/test', async (req, res) => {
+
+    const [rows] = await db.query('SELECT * FROM users');
+
+
     res.send({
         message: 'API test successful',
         method: req.method,
-        url: req.url
+        url: req.url,
+        users: rows
     });
 });
 
